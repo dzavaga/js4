bb6d6c9f6a1d2a463eb88556afe5f535488c4276
cipher <mishaofis86@gmail.com>
2023-04-25 18:29:45 +0300

    code

--- main.js
+++ main.js
@@ -23,3 +23,22 @@ try {
   console.log("Error:", error.message);
 }
 // 2
+function checkAge() {
+  try {
+    let name = prompt("Please enter your name:");
+    let age = prompt("Please enter your age:");
+    let status = prompt("Please enter your status (admin, moderator, user):");
+
+    if (age === "") throw new Error("The field is empty! Please enter your age");
+    if (isNaN(age)) throw new TypeError("The age must be a number");
+    if (age < 18 || age > 70) throw new RangeError("You are not allowed to watch the movie due to your age");
+    if (status !== "admin" && status !== "moderator" && status !== "user") throw new EvalError("Invalid status");
+
+    alert("You are allowed to watch the movie");
+
+  } catch (error) {
+    alert(`${error.name}: ${error.message}`);
+  }
+}
+checkAge();
+
